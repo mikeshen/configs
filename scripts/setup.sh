f4a1fc6cb7788a42cb20758be1dc29d4a03ebe38
Mike Shen <mikeshen@umich.edu>
2016-12-11 17:59:30 -0600

    added setup script + tmux confs

---
+++ scripts/setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# add repos
+sudo apt-add-repository -y "deb http://repository.spotify.com stable non-free"
+sudo add-apt-repository -y "deb http://linux.dropbox.com/ubuntu $(lsb_release -sc) main"
+sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
+sudo add-apt-repository ppa:jonathonf/vim
+
+# basic update
+sudo apt -y --force-yes update
+sudo apt -y --force-yes upgrade
+
+# install applications
+sudo apt -y --allow-unauthenticated install \
+	git \
+	spotify-client
+
+# remove folders
+rm -rf ~/Documents
+rm -rf ~/Public
+rm -rf ~/Templates
+rm -rf ~/Music
+rm -rf ~/Videos
+
+# add folders
+mkdir ~/Development
