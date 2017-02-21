372f68292e0a0761e8e472dd86d5b625ad481f56
Mike Shen <mikeshen@umich.edu>
2017-02-20 21:10:49 -0600

    renamed install script

---
+++ scripts/install-oh-my-zsh.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# install oh-my-zsh
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# set oh-my-zsh as the default shell
+chsh -s /bin/zsh
--- scripts/setup-oh-my-zsh.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# install oh-my-zsh
-curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh
-
-# set oh-my-zsh as the default shell
-chsh -s /bin/zsh
