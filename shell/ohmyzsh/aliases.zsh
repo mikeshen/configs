e859b22f92d42db438adf9fcc9251167f55bbefa
Mike Shen <mikeshen@umich.edu>
2017-07-19 23:36:19 -0500

    sync up alias files

--- shell/ohmyzsh/aliases.zsh
+++ shell/ohmyzsh/aliases.zsh
@@ -6,23 +6,34 @@
 alias cl='clear'
 alias :q='exit'
 alias :Q='exit'
-alias p='pushd'                                                              # push directory to stack and go to new dir
-alias o='popd'                                                               # pop to last directory. Kind of like a back button
-alias m='man'                                                                # i'm lazy af, ok?
+alias p='pushd'                                                                               # push directory to stack and go to new dir
+alias o='popd'                                                                                # pop to last directory. Kind of like a back button
+alias m='man'                                                                                 # i'm lazy af, ok?
 alias tmux="tmux -2"
 alias tm='tmux attach || tmux new'
 
 # tools
-alias more='less --shift 5 --ignore-case --chop-long-lines --RAW-CONTROL-CHARS --LONG-PROMPT'
-alias more='less'
-alias du='du -sh'                                                            # disk usage
-alias dusort='command du -s * .* | sort -n'                                  # disk usage w/ sort
-alias wcl='wc -l'                                                            # count line of text
-alias shred='shred --remove'                                                 # really remove
-alias gz='tar -xvzf'                                                         # unzips a tar gzipped file
-alias view='pygmentize -g'
-alias proc='ps aux | grep'
+alias du='du -sh'                                                                             # disk usage
+alias dusort='command du -s * .*|sort -n'                                                     # disk usage w/ sort
+alias wcl='wc -l'                                                                             # count line of text
+alias shred='shred --remove'                                                                  # really remove
+alias gz='tar -xvzf'                                                                          # unzips a tar gzipped file
+alias ctags='ctags -R --c++-kinds=+p --extra=+fq'                                             # correct recursive ctags
+alias proc='ps aux|grep'
 alias rsync='rsync -arP'
+
+# view
+alias less='less --shift 5 --ignore-case --chop-long-lines --RAW-CONTROL-CHARS --LONG-PROMPT'
+alias more='less'
+alias view='pygmentize -g'
+alias vhist='history|vless'
+
+# config aliases
+alias valias="$EDITOR ~/.oh_my_zsh/.aliases"
+alias vzsh="$EDITOR ~/.zshrc"
+alias vvim="$EDITOR ~/.vimrc"
+alias remind='cat $ZSH/.aliases.zsh|less'                                                     # remind me what the aliases were again?
+alias remindgit='cat ~/.gitconfig|less'                                                       # remind me what the git config was again?
 
 ########################################################################################################################
 # Build Tools
@@ -31,42 +42,37 @@ alias rsync='rsync -arP'
 alias mm='make'
 alias mc='make clean'
 alias mr='make run'
-alias rctags='ctags -R --c++-kinds=+p --extra=+fq'                           # recursively run ctags with c++ info
-alias gg='grep -nIi --exclude-dir ".git" --exclude "*~" --color'
-alias ggr='grep -rnIi --exclude-dir ".git" --exclude "*~" --color'
-alias agg='ag -SR --cpp --ignore=.git'
 
 ########################################################################################################################
 # FILES / EDITING
 ########################################################################################################################
 
-# editing shortcuts
-alias vvim='$EDITOR ~/.vimrc'
-alias vtmux='$EDITOR ~/.tmux.conf'
-alias vbash='$EDITOR ~/.bashrc'
-alias vzsh='$EDITOR ~/.zshrc'
-alias valias='$EDITOR $ZSH/.aliases.zsh'
-
-alias remind='cat $ZSH/.aliases.zsh|less'                                    # remind me what the aliases were again?
-
 # verbose file/dir movement
-alias rm='rm -v'
+alias rm='rm -vI'
 alias mv='mv -v'
 
+# ls shortcuts
 alias l='ls -alFp'
+alias la='ls -A'
+alias lm='ls -alhrt' # show last modified file by reverse order
+
+# search shortcuts
+alias gg='grep -nIi --exclude-dir={.git,.svn} --exclude "*~" --color'
+alias ggr='grep -rnIi --exclude-dir={.git,.svn} --exclude "*~" --color'
+alias agg='ag -SR --cpp --ignore={.git,.svn}'
+alias cg="find -L -regex '.*\.\(C\|H\|c\|h\|cpp\|hpp\)$' -not -regex '.*git.*' | xargs egrep --line-number"
+alias xg="find -L regex '.*\.\(xml\)$' | xargs egrep --line-number"
+alias pygrep-"find . -name '*.py' | xargs grep --line-number"
 
 # cd shortcuts
-alias c='cd'
-alias cc='cd $_' # got to last input arg, useful after running, say,
-                 # "mkdir long/dir/name/here", then just cc to go the newly created directory
-alias cb='cd -'
+alias c='cd'         # famous / infamous at this point
+alias cc='cd $_'     # got to last input arg, useful after running, say,
+                     # "mkdir long/dir/name/here", then just cc to go the newly created directory
+		     #
+alias cb='cd -'      # go to the last directory you just visited
 alias ..='cd ../'
 alias ...='cd ../..'
 alias ....='cd ../../..'
-
-# specific cd directories
-alias config='cd ~/Development/configs'
-alias dev='cd ~/Development
 
 ########################################################################################################################
 # GIT Shortcuts
@@ -76,37 +82,62 @@ alias gs='git status'
 alias gst='git status -uno'
 alias gcl='git clone'
 alias gb='git branch'
+alias gb='git branch --list'
+alias gch='git checkout'
 alias gra='git remote add'
 alias grr='git remote rm'
-alias gch='git checkout'
 alias gdis='git checkout --'
+alias gunstage='git reset HEAD'
+alias gun='gunstage'
+alias gu='gunstage'
 alias gc='git commit'
 alias gco='git commit'
 alias gca='git commit -a'
 alias gcm='git commit -m'
+alias gh='git help'
+alias gl='git log'
 alias ga='git add' # stage
 alias gA='git add .' # stage all
-alias gu='git reset HEAD' # unstage
-alias gl='git log'
-alias gd='git diff'
+alias gpull='git pull'
+alias gpl='gpull'
+alias gp='gpull'
+alias gpush='git push'
+alias gps='gpush'
 alias gdiff='git diff'
+alias gd='gdiff'
 alias gdc='git diff --cached'
 alias gds='git diff --stat'
-
+# stash
+alias gss='git stash'
+alias gsp='git stash pop'
+alias gsl='git stash list'
+alias gsd='git stash drop'
+alias gfp='git format-patch'
+alias gammend='git reset --soft HEAD~1'
+alias gnuke='git reset --hard HEAD~1'
+alias gdt='git difftool'
+alias gdifflast='git diff HEAD~1'
 # gsh shows the number of commits for the current repos for all developers
-alias gsh="git shortlog | grep -E '^[ ]+\w+' | wc -l"
-# gu shows a list of all developers and the number of commits they've made
-alias gu="git shortlog | grep -E '^[^ ]'"
+alias gcount="git shortlog | grep -E '^[ ]+\w+' | wc -l"
 
 ########################################################################################################################
 # VIM
 ########################################################################################################################
+
 # O - vertical splits
-# X - don't connect ot X window
+# X - do not connect to X window
 # u - custom vimrc
 # --cmd - run cmd
 
+alias f='v $(fzf)'
 alias vim='vim -O -X'
 alias v='vim'
 alias rmswap='rm $HOME/Development/configs/vim/cache/swap/*'
 
+########################################################################################################################
+# CUSTOM DIRS
+########################################################################################################################
+
+# specific cd directories
+alias config='cd ~/Development/configs'
+alias dev='cd ~/Development'
--- shell/ohmyzsh/setopt.zsh
+++ shell/ohmyzsh/setopt.zsh
@@ -21,6 +21,12 @@ setopt hist_verify # don't execute, just expand history
 setopt always_to_end # When completing from the middle of a word, move the cursor to the end of the word    
 setopt auto_menu # show completion menu on successive tab press. needs unsetop menu_complete to work
 setopt auto_name_dirs # any parameter that is set to the absolute name of a directory immediately becomes a name for that directory
+setopt chase_links # resolve symlinks
 setopt complete_in_word # Allow completion from within a word/phrase
 
+### Other ###
 unsetopt menu_complete # do not autoselect the first completion entry
+setopt print_exit_value # print return value if non-zero
+unsetopt hist_beep # no bell on error in history
+unsetopt list_beep # no bell on ambiguous completion
+unsetopt rm_star_silent # ask for confirmation on rm * style commands
