9399f08531c8bc1457437a371df72609aee533f4
Mike Shen <mikeshen@umich.edu>
2019-01-18 13:18:10 -0600

    add nice bash sec->readable time converter

--- shell/ohmyzsh/funcs.zsh
+++ shell/ohmyzsh/funcs.zsh
@@ -85,6 +85,20 @@ function trl() {
     tr "$delimiter" "|"
 }
 
+# convert time to days+hours+minutes+seconds
+function displaytime {
+    local T=$1
+    local D=$((T/60/60/24))
+    local H=$((T/60/60%24))
+    local M=$((T/60%60))
+    local S=$((T%60))
+    [[ $D > 0 ]] && printf '%d days ' $D
+    [[ $H > 0 ]] && printf '%d hours ' $H
+    [[ $M > 0 ]] && printf '%d minutes ' $M
+    [[ $D > 0 || $H > 0 || $M > 0 ]] && printf 'and '
+  printf '%d seconds\n' $S
+}
+
 # view next commit after the current checkout
 function gnext() {
     git log --reverse --pretty=%H master | grep -A 1 $(git rev-parse HEAD) | tail -n1 | xargs git checkout
