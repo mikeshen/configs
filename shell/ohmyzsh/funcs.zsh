7203c48343203ec8018eaee52864ea740ffbb296
Mike Shen <mikeshen@umich.edu>
2019-01-30 17:21:58 -0600

    add dir to ff and fl zsh funcs, add additional cit scripts

--- shell/ohmyzsh/funcs.zsh
+++ shell/ohmyzsh/funcs.zsh
@@ -39,12 +39,24 @@ function ld-path-prepend() {
 }
 
 function ff() {
-    find . -type f ! -ipath "*.git*" ! -ipath "*bin*" -iwholename '*'$*'*' ;
+    if [[ -n $1 ]]; then
+        dir=$2
+    else
+        dir='.'
+    fi
+
+    find $2 -type f ! -ipath "*.git*" ! -ipath "*bin*" -iwholename '*'$1'*' ;
 }
 
 function fl() {
-    find . ! -ipath "*.git*" ! -ipath "*bin*" -type f -iname '*'"$*"'*' -ls | awk '{print $NF}'
+    if [[ -n $1 ]]; then
+        dir=$2
+    else
+        dir='.'
+    fi
+    find $2 ! -ipath "*.git*" ! -ipath "*bin*" -type f -iname '*'"$1"'*' -ls | awk '{print $NF}'
 }
+
 # check if a process is running
 function psg() {
     ps ax | grep "$1" | grep -v grep
@@ -85,6 +97,21 @@ function trl() {
     tr "$delimiter" "|"
 }
 
+# convert time from seconds to D/H/M/S from now
+function displaytime() {
+    # can alias blah=echo "$TIME - $(date +%s)"|bc -l|displaytime
+  local T=$1
+  local D=$((T/60/60/24))
+  local H=$((T/60/60%24))
+  local M=$((T/60%60))
+  local S=$((T%60))
+  (( $D > 0 )) && printf '%d days ' $D
+  (( $H > 0 )) && printf '%d hours ' $H
+  (( $M > 0 )) && printf '%d minutes ' $M
+  (( $D > 0 || $H > 0 || $M > 0 )) && printf 'and '
+  printf '%d seconds\n' $S
+}
+
 # view next commit after the current checkout
 function gnext() {
     git log --reverse --pretty=%H master | grep -A 1 $(git rev-parse HEAD) | tail -n1 | xargs git checkout
@@ -95,4 +122,69 @@ function sb {
     SOURCEFILE=$HOME/.zshrc
     echo "source $SOURCEFILE"
     source $SOURCEFILE
+}
+
+
+# Good Template Functions, here for reference
+
+function hfe() {
+    local number=$1
+    if [[ -n $2 ]]; then
+        user=$2
+    else
+        user='sshen'
+    fi
+
+    set -xv
+    host="$user@server$number"
+    ssh $host
+    set +xv
+}
+
+function getDate() {
+    date="$(date +%Y/%m/%d)"
+    echo $date
+}
+
+function getYear() {
+    # example of pulling substr out of a string using awk
+    # here we pull yr out of a line with yyyymmdd
+    [[ -n $1 ]] || return
+    local yr=`awk '{print substr($0,1,4) }' <<< $1`
+    echo $yr
+}
+
+function flash() {
+    [[ -n $1 ]] || return
+    local src=$1
+    local dst='s_rttqa@lq-njq-hfe10:/auto/rtt/common'
+    set -xv
+    rsync $src $dst
+    set +xv
+    # rsync'd, now add only the last foldername to the path we ssh to
+    if [[ -d $src ]]; then
+        src=`echo "$src"|sed "s|.*/||g"`
+        ssh -t s_rttqa@lq-njq-hfe10 "cd /auto/rtt/common/$src; exec \\$SHELL --login"
+    elif [[ -f $src ]]; then
+        ssh -t s_rttqa@lq-njq-hfe10 "cd /auto/rtt/common/; exec \\$SHELL --login"
+    fi
+}
+
+function dl() {
+    if [[ ! -n $1 ]]; then
+        echo "saves logs from blah"
+        echo "dl {yyyymmdd} {keyword}"
+        return
+    elif [[ ! -n $2 ]]; then
+        echo "saves logs from blah"
+        echo "dl {yyyymmdd} {keyword}"
+        return
+    fi
+
+    # extract each file
+    for var in $(find $(1) -type f -iname '*'"$2"'*.xz' -ls | awk '{print $NF}')
+    do
+        local src="`echo $var|sed "s|.*/||g"|sed "s|\.xz||g"`"
+        xz -dk --stdout $var > $src
+    done
 }
