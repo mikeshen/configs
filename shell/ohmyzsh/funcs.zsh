68bf9e05927b5716efb7054d39d63e13d2209ad7
Mike Shen <mikeshen@umich.edu>
2019-01-30 17:14:03 -0600

    fix whitespace in zsh/funcs.zsh

--- shell/ohmyzsh/funcs.zsh
+++ shell/ohmyzsh/funcs.zsh
@@ -2,81 +2,81 @@
 
 # remove item from $PATH
 function path-remove () {
-  local IFS=':'
-  local NEWPATH
-  for DIR in $PATH; do
+    local IFS=':'
+    local NEWPATH
+    for DIR in $PATH; do
     if [ "$DIR" != "$1" ]; then
-      NEWPATH=${NEWPATH:+$NEWPATH:}$DIR
+        NEWPATH=${NEWPATH:+$NEWPATH:}$DIR
     fi
-  done
-  export PATH=${NEWPATH};
+    done
+    export PATH=${NEWPATH};
 }
 
 # add item to end of $PATH, uniquely
 function path-append() {
-  [ -d $1 ] || return 1    # make sure directory exists
-  # path-remove $1           # remove the directory
-  export PATH=${PATH}:${1} # append the directory
+    [ -d $1 ] || return 1    # make sure directory exists
+    # path-remove $1           # remove the directory
+    export PATH=${PATH}:${1} # append the directory
 }
 
 # add item to beginning of $PATH, uniquely
 function path-prepend() {
-  [ -d $1 ] || return 1     # make sure directory exists
-  # path-remove $1            # remove the directory
-  export PATH=${1}:${PATH}  # append the directory
+    [ -d $1 ] || return 1    # make sure directory exists
+    # path-remove $1           # remove the directory
+    export PATH=${1}:${PATH} # append the directory
 }
 
 # add item to end of $PATH, uniquely
 function ld-path-append() {
-  [ -d $1 ] || return 1    # make sure directory exists
-  export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${1} # append the directory
+    [ -d $1 ] || return 1    # make sure directory exists
+    export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${1} # append the directory
 }
 
 # add item to beginning of $PATH, uniquely
 function ld-path-prepend() {
-  [ -d $1 ] || return 1     # make sure directory exists
-  export LD_LIBRARY_PATH=${1}:${LD_LIBRARY_PATH}  # append the directory
+    [ -d $1 ] || return 1    # make sure directory exists
+    export LD_LIBRARY_PATH=${1}:${LD_LIBRARY_PATH}  # append the directory
 }
 
 function ff() {
-  find . -type f ! -ipath "*.git*" ! -ipath "*bin*" -iwholename '*'$*'*' ;
+    find . -type f ! -ipath "*.git*" ! -ipath "*bin*" -iwholename '*'$*'*' ;
 }
 
 function fl() {
-  find . ! -ipath "*.git*" ! -ipath "*bin*" -type f -iname '*'"$*"'*' -ls | awk '{print $NF}'
+    find . ! -ipath "*.git*" ! -ipath "*bin*" -type f -iname '*'"$*"'*' -ls | awk '{print $NF}'
 }
 # check if a process is running
 function psg() {
-  ps ax | grep "$1" | grep -v grep
+    ps ax | grep "$1" | grep -v grep
 }
 
 # awk fields quickly
 function ac() {
-  local args="\$$1"
-  local noise="noise"
-  for var in "$@"
-  do
-    if [[ ! -z "$noise" ]]; then
-      noise=""
-    else
-      args="$args\" \"\$$var"
-    fi
-  done
-  awk -F'|' "{print $args}"
+    local args="\$$1"
+    local noise="noise"
+    for var in "$@"
+    do
+        if [[ ! -z "$noise" ]]; then
+            noise=""
+        else
+            args="$args\" \"\$$var"
+        fi
+    done
+    awk -F'|' "{print $args}"
 }
 
 function acl() {
-  local args="\$$1"
-  local noise="noise"
-  for var in "$@"
-  do
-    if [[ ! -z "$noise" ]]; then
-      noise=""
-    else
-      args="$args\"\\n\"\$$var"
-    fi
-  done
-  awk -F'|' "{print $args}"
+    local args="\$$1"
+    local noise="noise"
+    for var in "$@"
+    do
+        if [[ ! -z "$noise" ]]; then
+            noise=""
+        else
+            args="$args\"\\n\"\$$var"
+        fi
+    done
+      awk -F'|' "{print $args}"
 }
 
 # tr any delimiter to |
@@ -92,7 +92,7 @@ function gnext() {
 
 # source bashrc
 function sb {
-  SOURCEFILE=$HOME/.zshrc
-  echo "source $SOURCEFILE"
-  source $SOURCEFILE
+    SOURCEFILE=$HOME/.zshrc
+    echo "source $SOURCEFILE"
+    source $SOURCEFILE
 }
